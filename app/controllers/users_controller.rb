58be4c0f4a4092c967afe4a16bae3e5318489b7d
Dyego Rodrigo <dyego9444@hotmail.com>
2014-12-01 00:01:12 -0200

    Dynamic app title

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 	end
 	private
 	def user_params
-		params.require(:user).permit(:name,:email,:password)
+		params.require(:user).permit(:name,:email,:password,:password_confirmation)
 	end
 end
 
