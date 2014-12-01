e4a24d7ba8c26271299636d3a4249af4942390aa
Dyego Rodrigo <dyego9444@hotmail.com>
2014-12-01 17:48:02 -0200

    Email has to be unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130202335) do
+ActiveRecord::Schema.define(version: 20141201193452) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20141130202335) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
