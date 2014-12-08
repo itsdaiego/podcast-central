c3af06e37e51838eccaf29898b00d6c84bbef83c
Dyego Rodrigo <dyego9444@hotmail.com>
2014-12-08 18:50:30 -0200

    Log in/Sign in fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-
+  include SessionsHelper
   def new
   end
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+    include SessionsHelper
 	def new
 		@user = User.new
 	end
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			sign_in @user
 			flash[:success] = "User Created!"
 			redirect_to @user
 		else
