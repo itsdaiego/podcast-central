ab3508e663b27a5ffd6afe53d0dd205a49ebfa80
Dyego Rodrigo <dyego9444@hotmail.com>
2014-12-13 00:13:02 -0200

    More tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,8 +18,37 @@ describe User do
 
 	it { should be_valid }
 
-	describe "when name is not present" do
+	describe "when name is empty" do
 		before { @user.name = " " }
 		it { should_not be_valid }
+	end
+
+	describe "when email is empty" do
+		before {@user.email = " "}
+		it { should_not be_valid }
+	end
+	describe "when name is too long (over 50 characters)" do
+		before { @user.name = "password" *51 }
+		it { should_not be_valid }
+	end
+	describe "when user attempt to take an email that was already taken" do
+		before do
+			user_email = @user.dup
+			user_email.email = @user.email.upcase
+			user_email.save
+		end
+		it { should_not be_valid }
+	end
+	describe "when password confirmation is wrong" do
+		before { @user.password_confirmation = "wrongpassword" }
+		it { should_not be_valid }
+	end
+	describe "when password is too short" do
+		before { @user.password = "a" * 5}
+		it { should_not be_valid }
+	end
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 end	
