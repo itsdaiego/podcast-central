1b1da76d7e2debb32b31c3cf6c20ce1b2c888c38
Dyego Rodrigo <dyego9444@hotmail.com>
2014-12-14 21:17:24 -0200

    Selector fix for dropdown

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("#dropdown-menu").click(function(e){
+	$("a#dropdown-menu").click(function(e){
 		e.preventDefault();
 		$("ul.submenu").slideToggle("slow");
 		console.log("menu dropdown is working");
