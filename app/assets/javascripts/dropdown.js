a612b441026629fc4153adf4d98cbf4950233acc
Dyego Rodrigo <dyego9444@hotmail.com>
2015-01-02 03:53:47 -0200

    menu for mobile devices

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+	//Desktop Version
 	$("a#dropdown-menu").click(function(e){
 		e.preventDefault();
 		$("ul.submenu").slideToggle("slow");
 		console.log("dropdown menu is working");
+	});
+	//Mobile version
+	$("ul.menu-icon").click(function(e){
+		e.preventDefault();
+		$("ul.menu-right").toggle();
+		console.log("dropdown menu for mobile devices is working");
 	});
 });
 
