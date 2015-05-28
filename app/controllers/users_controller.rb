0a06852e97032f94e29fc2c1cbc160efaadf4d81
Dyego Rodrigo <dyego9444@hotmail.com>
2015-05-27 21:13:13 -0300

    followers/following page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 	before_action :signed_in_user,
-	only: [:index, :edit, :update, :destroy]
+	only: [:index, :edit, :update, :destroy, :following, :followers]
 
 	before_action :correct_user,   only: [:edit, :update]
- 
+
 	def index
 		@users = User.paginate(page: params[:page]).per_page(5)
 	end
@@ -34,6 +34,20 @@ class UsersController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	def following
+		@title = "Following"
+		@user  = User.find(params[:id])
+		@users = @user.following.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
+	def followers
+		@title = "Followers"
+		@user  = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
 	end
 	private
 	def user_params
