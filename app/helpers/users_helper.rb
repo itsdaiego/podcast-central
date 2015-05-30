d7cd433a6cec807d5f32c0adfc8adfde76db90d9
Dyego Rodrigo <dyego9444@hotmail.com>
2015-05-30 19:26:15 -0300

    adding some style for follower/following pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user (sample_app_rails 4).
-  def gravatar_for(user, options = { size: 50 })
+  def gravatar_pic(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
