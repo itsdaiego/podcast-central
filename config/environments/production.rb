38c2a548d104586e9ba33cb05f3d2cd19d69d5c9
Dyego Rodrigo <dyego9444@hotmail.com>
2015-07-25 16:54:26 -0300

    config fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,17 +62,17 @@ PodcastCentral::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  #config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  #config.active_support.deprecation = :notify
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  #config.log_formatte
 
   # Do not dump schema after migrations.
-  #config.active_record.dump_schema_after_migration = false
+  #
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,17 +61,16 @@ ActiveRecord::Schema.define(version: 20150628192058) do
     t.integer "user_id"
   end
 
-  create_table "podcasts_users_joins", id: false, force: true do |t|
-    t.integer "podcast_id"
-    t.integer "user_id"
-  end
-
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
